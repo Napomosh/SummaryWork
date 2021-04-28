aefcec73b4f456975ec37ecdebcb7b643cb894b9
Alexander <sashulik999@gmail.com>
2021-04-28 18:21:45 +0300

    Add tclap

--- Summary/scripts/run.sh
+++ Summary/scripts/run.sh
@@ -1,2 +1,2 @@
 cd build/
-./main
+./main -l ../resources/lab19.pdf -r ../resources/json_file.json
--- Summary/src/main.cpp
+++ Summary/src/main.cpp
@@ -2,29 +2,59 @@
 #include <string>
 #include <../libs/pdftron/PDF/PDFNet.h>
 
+#include "../libs/tclap/CmdLine.h"
+
 #include "Parser.h"
 
 using namespace pdftron;
+using namespace TCLAP;
 
 int main(int argc, char* argv[])
 {
 	int ret = 0;
 	Parser parser;
-	PDFNet::Initialize();
-	if (argc > 1)
-	{
-	    for(int i = 1; i < argc; ++i)
-	    {
-	    	parser.set_file(argv[i]);
-	    }
-	}
-	else
-	{
-		std::string input_path { "../resources/lab7.pdf" };
-		parser =  Parser(input_path);
-	}
 	try
 	{
+		// Define the command line object.
+		CmdLine cmd("Command description message", ' ', "1.0");
+
+		// Define a value argument and add it to the command line.
+		ValueArg<std::string> lab_name("l","lab","Name of lab file",true,"homer","string");
+		cmd.add( lab_name );
+
+		// Define a switch and add it to the command line.
+		ValueArg<std::string> rules_name("r","rule","Name of rule file",true,"homer","string");
+		cmd.add( rules_name );
+
+		// Parse the args.
+		cmd.parse( argc, argv );
+
+		// Get the value parsed by each arg. 
+		std::string input_path = lab_name.getValue();
+		std::string rule_path = rules_name.getValue();
+		parser = Parser(input_path, rule_path);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		ret = 1;
+	}
+	
+	// if (argc > 1)
+	// {
+	//     for(int i = 1; i < argc; ++i)
+	//     {
+	//     	parser.set_file(argv[i]);
+	//     }
+	// }
+	// else
+	// {
+	// 	std::string input_path { "../resources/lab7.pdf" };
+	// 	parser =  Parser(input_path);
+	// }
+	try
+	{
+		PDFNet::Initialize();
     	parser.parse();
 	}
 	catch (std::exception& e)
