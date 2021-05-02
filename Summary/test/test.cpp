ed59f45aa5c4dbd3b844714f3ff6931dde909716
Alexander <sashulik999@gmail.com>
2021-05-02 21:12:00 +0300

    Add tests

--- Summary/test/test.cpp
+++ Summary/test/test.cpp
@@ -10,65 +10,33 @@ TEST_CASE("Check lab7.pdf") {
 	PDFNet::Initialize();
 	std::string input_path { "../../resources/lab7.pdf" };
 	Parser parser =  Parser(input_path, "../../test/json_file.json");
-	Checker checker = parser.parse();
-
-	auto messages = checker.get_head_messages();
-	std::list<std::string> expected { "Найден заголовок: Задание", "Найден заголовок: Инструкция пользователя", "Найден заголовок: Тестирование" };
-	auto it_expected = expected.begin();
-	int exp_size = expected.size();
-
-	CHECK(messages.size() == exp_size);	
-	for(auto it = messages.begin(); it != messages.end(); ++it)
-	{
-		CHECK(*it == *it_expected);
-		++it_expected;
-	}
-
-	messages = checker.get_style_messages();
-	CHECK(messages.size() == 15);
-
-	messages = checker.get_test_messages();
-	expected = { "Тест номер: 1 найден.", "Тест номер: 2 найден.", "Тест номер: 3 найден." };
-	it_expected = expected.begin();
-	exp_size = expected.size();
-
-	CHECK(messages.size() == exp_size);	
-	for(auto it = messages.begin(); it != messages.end(); ++it)
-	{
-		CHECK(*it == *it_expected);
-		++it_expected;
-	}	
-
-	PDFNet::Terminate();
+	CHECK_EQ(parser.parse(), parser.SUCCESS);
 }
 
-// TEST_CASE("Check style rules 1") {
-// 	std::string input_path { "../../resources/lab7.pdf" };
-// 	Parser parser =  Parser(input_path, "../../test/json_file.json");
-// 	Checker checker = parser.parse();
+TEST_CASE("Check lab19.pdf")
+{
+	std::string input_path{"../../resources/lab19.pdf"};
+	Parser parser = Parser(input_path, "../../test/json_file.json");
+	CHECK_EQ(parser.parse(), parser.SUCCESS);
+}
 
-// 	auto messages = checker.get_style_messages();
-// 	CHECK(messages.size() == 15);	
+TEST_CASE("Check lab20.pdf")
+{
+	std::string input_path{"../../resources/lab20.pdf"};
+	Parser parser = Parser(input_path, "../../test/json_file.json");
+	CHECK_EQ(parser.parse(), parser.SUCCESS);
+}
 
-// 	// PDFNet::Terminate();
-// }
+TEST_CASE("Check lab21.pdf")
+{
+	std::string input_path{"../../resources/lab21.pdf"};
+	Parser parser = Parser(input_path, "../../test/json_file.json");
+	CHECK_EQ(parser.parse(), parser.SUCCESS);
+}
 
-// TEST_CASE("Check test rules 1") {
-// 	std::string input_path { "../../resources/lab7.pdf" };
-// 	Parser parser =  Parser(input_path, "../../test/json_file.json");
-// 	Checker checker = parser.parse();
-
-// 	auto messages = checker.get_test_messages();
-// 	std::list<std::string> expected { "Тест номер: 1 найден.", "Тест номер: 2 найден.", "Тест номер: 3 найден." };
-// 	auto it_expected = expected.begin();
-// 	int exp_size = expected.size();
-
-// 	CHECK(messages.size() == exp_size);	
-// 	for(auto it = messages.begin(); it != messages.end(); ++it)
-// 	{
-// 		CHECK(*it == *it_expected);
-// 		++it_expected;
-// 	}	
-
-// 	PDFNet::Terminate();
-// }
+TEST_CASE("Check lab18.pdf") {
+	std::string input_path { "../../resources/lab18.pdf" };
+	Parser parser =  Parser(input_path, "../../test/json_file.json");
+	CHECK_EQ(parser.parse(), parser.SUCCESS);
+	PDFNet::Terminate();
+}
